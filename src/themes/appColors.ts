4fc688799414836debf690ee19c91514418f04d9
Kemal Koçbeler <kkocbeler@gmail.com>
2026-04-16 17:21:19 +0300

    fix typo and update app colors

--- src/themes/appColors.ts
+++ src/themes/appColors.ts
@@ -1,13 +1,16 @@
 export const APP_COLORS = {
-  light: "#E6E6E6",
-  accent: "#C5A880",
-  brown: "#532E1C",
-  dark: "#0F0F0F",
+  light: "#FFFFFF",
+  primary: "#e65705",
+  secondary: "#f0ebe8",
+  accent: "#0F0F0F",
   black: "#000000",
   white: "#FFFFFF",
   textDark: "#0F0F0F",
   textWhite:  "#FFFFFF",
-  textGray: "#9A9A9A",
-  border: "#D6D6D6",
-  appBackground: "#FAFAFA"
+  textGray: "#5f5f5f",
+  backgroundGray: "#eceaea",
+  border: "#F6F6F6", //kart üstüneki kart
+  appBackground: "#FCFBFC",
+  danger: "#831616",
+  star: "#c4c112"
 };
--- src/themes/appTypography.ts
+++ src/themes/appTypography.ts
@@ -2,9 +2,9 @@ import { moderateScale } from "react-native-size-matters";
 
 export const APP_TYPOGRAPHY = {
   sizes: {
-    xs: moderateScale(12),
-    sm: moderateScale(14),
-    md: moderateScale(16),
+    xs: moderateScale(4),
+    sm: moderateScale(8),
+    md: moderateScale(14),
     lg: moderateScale(18),
     xl: moderateScale(22),
     xxl: moderateScale(28),
@@ -26,28 +26,19 @@ export const APP_TYPOGRAPHY = {
   variants: {
     title: {
       fontSize: moderateScale(22),
-      fontWeight: "700",
       lineHeight: moderateScale(28),
     },
     subtitle: {
       fontSize: moderateScale(18),
-      fontWeight: "500",
-      lineHeight: moderateScale(24),
     },
     body: {
       fontSize: moderateScale(16),
-      fontWeight: "400",
-      lineHeight: moderateScale(22),
     },
     caption: {
       fontSize: moderateScale(14),
-      fontWeight: "400",
-      lineHeight: moderateScale(18),
     },
     small: {
       fontSize: moderateScale(12),
-      fontWeight: "400",
-      lineHeight: moderateScale(16),
     },
   },
 };
