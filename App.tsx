314e012df4856dfa97a05fb6ad97bedebfb6210a
Kemal Koçbeler <kkocbeler@gmail.com>
2026-04-16 17:22:21 +0300

    add context provider to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import MainAppStack from './src/navigation/MainAppStack'
+import { Provider } from 'react-redux'
+import { store } from './src/store/store'
+import 'react-native-get-random-values';
+import { WishlistProvider } from './src/context/WishlistContext'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+        <NavigationContainer>
+            <WishlistProvider>
+                <MainAppStack />
+            </WishlistProvider>
+        </NavigationContainer>
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
